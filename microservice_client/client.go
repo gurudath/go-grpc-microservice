3f3ea593bc40e33118aecde5b6bdbc4040da9710
gurudath bn <gurudath.1989@gmail.com>
2019-03-25 10:26:41 +0530

    first commit Unary API

---
+++ microservice_client/client.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go-grpc-microservice/microservice/microservicepb"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hellow i am client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not comment: %v\n", err)
+	}
+	defer cc.Close()
+
+	c := microservicepb.NewMicroserviceServiceClient(cc)
+	// fmt.Printf("Creating client: %f\n", c)
+	doUnary(c)
+
+}
+
+func doUnary(c microservicepb.MicroserviceServiceClient) {
+	fmt.Println("Starting Unary")
+	req := &microservicepb.MicroserviceRequest{
+		Microservice: &microservicepb.Microservice{
+			FirstName: "Gurudath",
+			LastName:  "BN",
+		},
+	}
+	res, err := c.Microservice(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error Client %v", err)
+	}
+	log.Printf("Response of Greeting: %v", res.Result)
+}
---
+++ microservice_server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go-grpc-microservice/microservice/microservicepb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (*server) Microservice(ctx context.Context, req *microservicepb.MicroserviceRequest) (*microservicepb.MicroserviceResponse, error) {
+	firstName := req.GetMicroservice().GetFirstName()
+	result := "Hellow " + firstName
+	res := &microservicepb.MicroserviceResponse{
+		Result: result,
+	}
+	log.Printf("Request of Greeting: %v", req)
+	return res, nil
+}
+
+func main() {
+	fmt.Println("Hello Guru")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	microservicepb.RegisterMicroserviceServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
